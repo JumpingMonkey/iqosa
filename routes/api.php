4f2af47a0ba412792e36bd10f6040cd86fc48683
AlexMorozov <morozovprav@gmail.com>
2021-07-20 18:06:12 +0300

    cors fix 2

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+header('Access-Control-Allow-Origin: *');
+
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Pages\AboutUsPageController;
 use App\Http\Controllers\Pages\BlogPageController;
@@ -48,9 +52,7 @@ use App\Http\Controllers\MemberController;
 //    });
 //});
 
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-header('Access-Control-Allow-Origin: *');
+
 
 Route::middleware('locale')->group(function (){
     Route::get('/main', [MainPageController::class, 'index']);
