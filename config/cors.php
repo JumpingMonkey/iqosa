b1308ae2b0ea92dfdef85c3543f21a57f5dfb222
AlexMorozov <morozovprav@gmail.com>
2021-07-20 17:22:05 +0300

    fix cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://backiqosa.staj.fun','*'],
 
     'allowed_origins_patterns' => [],
 
